94c558b2f9dc1ece09749218e5f422cdcf6a3c65
Benoit <benoit.theunissen@gmail.com>
2016-03-19 19:01:31 +0100

    fix text test name from attr

--- README.md
+++ README.md
@@ -19,9 +19,41 @@ With Result class POJO definition as follow :
 @Data
 public static class Result {
 
-	@JsoupSelect("#name")
+	@JsoupSelect("#nameId")
 	@JsoupText
 	public String name;
 
 }
 ```
+
+Giving this DOM HTML, the POJO's ```name``` field will be set with ```"This is my name" ```
+
+```html
+<html>
+	<head></head>
+	<body>
+		<div id="nameId">This is my name</div>
+	</body>
+</html>
+```
+
+# Annotation on methods
+
+JSoupMapper's annotations can be use directly on methods. This provides a way to implement custom operations.
+
+```java
+@Data
+public static class Result {
+
+	public String name;
+	
+	@JsoupSelect("#nameId")
+	@JsoupText
+	public void setName(String pName){
+		this.name = pName.substring(0,3).toUpperCase();
+	}
+
+}
+```
+ 
+
--- src/test/java/neomcfly/jsoupmapper/JSoupMapperTextTest.java
+++ src/test/java/neomcfly/jsoupmapper/JSoupMapperTextTest.java
@@ -9,33 +9,35 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class JSoupMapperTest {
+public class JSoupMapperTextTest {
 
     JsoupMapper mapper = new JsoupMapper();
 
     @Test
-    public void attr1() {
+    public void text1() {
 
-        InputStream document = read("/attr/u1.html");
+        InputStream document = read("/text/u1.html");
 
         Result result = mapper.fromBody(document, Result.class);
 
         TestCase.assertNotNull(result);
-        TestCase.assertNotNull(result.getName());
+        TestCase.assertNotNull(result.getName1());
+        TestCase.assertNotNull(result.getName2());
 
         log.info(result.toString());
 
     }
 
     @Test
-    public void attr2() {
+    public void text2() {
 
-        InputStream document = read("/attr/u2.html");
+        InputStream document = read("/text/u2.html");
 
         Result fromBody = mapper.fromBody(document, Result.class);
 
         TestCase.assertNotNull(fromBody);
-        TestCase.assertNotNull(fromBody.getName());
+        TestCase.assertNotNull(fromBody.getName1());
+        TestCase.assertNotNull(fromBody.getName2());
 
         log.info(fromBody.toString());
 
@@ -46,7 +48,15 @@ public class JSoupMapperTest {
 
         @JsoupSelect("#name")
         @JsoupText
-        public String name;
+        public String name1;
+
+        public String name2;
+
+        @JsoupSelect("#name")
+        @JsoupText
+        public void setName2(String name2) {
+            this.name2 = name2;
+        }
 
     }
 
