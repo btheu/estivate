dccabb3a93d0f7ed058696a9f6f424d1c7563234
Benoit <benoit.theunissen@gmail.com>
2016-04-02 21:50:57 +0200

    new testcase for @Text

--- src/test/java/estivate/TextTest.java
+++ src/test/java/estivate/TextTest.java
@@ -1,11 +1,11 @@
 package estivate;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import estivate.EstivateMapper;
 import estivate.annotations.Text;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +16,47 @@ public class TextTest {
     EstivateMapper mapper = new EstivateMapper();
 
     @Test
+    public void text2() {
+
+        InputStream document = read("/text/u2.html");
+
+        Result2 result = mapper.map(document, Result2.class);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals("Text1 Text2 Text3 child of 3", result.getCas1());
+        Assert.assertEquals("Text1 Text2 Text3", result.getCas2());
+
+        Assert.assertEquals(3, result.getCas3().size());
+        Assert.assertEquals("Text1", result.getCas3().get(0));
+        Assert.assertEquals("Text2", result.getCas3().get(1));
+        Assert.assertEquals("Text3 child of 3", result.getCas3().get(2));
+
+        Assert.assertEquals(3, result.getCas4().size());
+        Assert.assertEquals("Text1", result.getCas4().get(0));
+        Assert.assertEquals("Text2", result.getCas4().get(1));
+        Assert.assertEquals("Text3", result.getCas4().get(2));
+
+        log.info(result.toString());
+    }
+
+    @Data
+    public static class Result2 {
+
+        @Text(select = "span")
+        public String cas1;
+
+        @Text(select = "span", own = true)
+        public String cas2;
+
+        @Text(select = "span")
+        public List<String> cas3;
+
+        @Text(select = "span", own = true)
+        public List<String> cas4;
+
+    }
+
+    @Test
     public void text1() {
 
         InputStream document = read("/text/u1.html");
@@ -23,8 +64,8 @@ public class TextTest {
         Result result = mapper.map(document, Result.class);
 
         Assert.assertNotNull(result);
-        Assert.assertEquals("This is my name: John",result.getName1());
-        Assert.assertEquals("This is my name: John",result.getName2());
+        Assert.assertEquals("This is my name: John", result.getName1());
+        Assert.assertEquals("This is my name: John", result.getName2());
 
         log.info(result.toString());
 
@@ -51,12 +92,12 @@ public class TextTest {
     @Data
     public static class Result {
 
-        @Text(select="#name")
+        @Text(select = "#name")
         public String name1;
 
         public String name2;
 
-        @Text(select="#name")
+        @Text(select = "#name")
         public void setName2(String name2) {
             this.name2 = name2;
         }
@@ -66,12 +107,12 @@ public class TextTest {
     @Data
     public static class ResultOwn {
 
-        @Text(select="#name", own=true)
+        @Text(select = "#name", own = true)
         public String name1;
 
         public String name2;
 
-        @Text(select="#name", own=true)
+        @Text(select = "#name", own = true)
         public void setName2(String name) {
             this.name2 = name;
         }
