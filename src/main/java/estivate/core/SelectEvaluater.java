bd4b40ae02032ca960cdd7da60b8409e03c15b8b
Benoit <benoit.theunissen@gmail.com>
2016-04-03 11:32:56 +0200

    add test for recursive mapping with simple String

--- src/main/java/estivate/core/SelectEvaluater.java
+++ src/main/java/estivate/core/SelectEvaluater.java
@@ -22,127 +22,141 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class SelectEvaluater {
 
-	public static Elements select(SelectBean bean, Elements elementsIn, AnnotatedElement member) {
+    public static Elements select(SelectBean bean, Elements elementsIn,
+            AnnotatedElement member) {
 
-		log.debug("selecting '{}' with '{}' for '{}'",bean.getSelect(), bean.getName(), ClassUtils.getName(member));
-		log.trace("select in '{}'", elementsIn.toString());
+        String select = bean.getSelect();
+        if (StringUtil.isBlank(select)) {
+            log.debug("no select expr on '{}' for '{}'", bean.getName(),
+                    ClassUtils.getName(member));
+            return elementsIn;
+        }
 
-		Elements elementsOut = elementsIn.select(bean.getSelect());
+        log.debug("selecting '{}' with '{}' for '{}'", select, bean.getName(),
+                ClassUtils.getName(member));
+        log.trace("select in '{}'", elementsIn.toString());
 
+        Elements elementsOut = elementsIn.select(select);
 
-		if(bean.isFirst() && bean.isLast()){
-			throw new IllegalArgumentException("Select cant be true for first() and last() a the same time");
-		}
-		if(bean.isFirst()){
-			log.trace("select first element");
-			elementsOut = new Elements(elementsOut.first());
-		}
-		if(bean.isLast()){
-			log.trace("select last element");
-			elementsOut = new Elements(elementsOut.last());
-		}
-		log.trace("select out '{}'", elementsOut.toString());
-		
-		return elementsOut;
-	}
+        if (bean.isFirst() && bean.isLast()) {
+            throw new IllegalArgumentException(
+                    "Select cant be true for first() and last() a the same time");
+        }
+        if (bean.isFirst()) {
+            log.trace("select first element");
+            elementsOut = new Elements(elementsOut.first());
+        }
+        if (bean.isLast()) {
+            log.trace("select last element");
+            elementsOut = new Elements(elementsOut.last());
+        }
+        log.trace("select out '{}'", elementsOut.toString());
 
+        return elementsOut;
+    }
 
-	public static Elements select(Text annotation, Elements elements, AnnotatedElement member) {
-		return select(new SelectBean(annotation),elements, member);
-	}
-	public static Elements select(Select annotation, Elements elements, AnnotatedElement member) {
-		return select(new SelectBean(annotation),elements, member);
-	}
-	public static Elements select(Attr annotation, Elements elements, AnnotatedElement member) {
-		return select(new SelectBean(annotation),elements, member);
-	}
-	public static Elements select(TagName annotation, Elements elements, AnnotatedElement member) {
-		return select(new SelectBean(annotation),elements, member);
-	}
-	public static Elements select(Val annotation, Elements elements, AnnotatedElement member) {
-		return select(new SelectBean(annotation),elements, member);
-	}
+    public static Elements select(Text annotation, Elements elements,
+            AnnotatedElement member) {
+        return select(new SelectBean(annotation), elements, member);
+    }
 
+    public static Elements select(Select annotation, Elements elements,
+            AnnotatedElement member) {
+        return select(new SelectBean(annotation), elements, member);
+    }
 
+    public static Elements select(Attr annotation, Elements elements,
+            AnnotatedElement member) {
+        return select(new SelectBean(annotation), elements, member);
+    }
 
-	@Data
-	public static class SelectBean {
+    public static Elements select(TagName annotation, Elements elements,
+            AnnotatedElement member) {
+        return select(new SelectBean(annotation), elements, member);
+    }
 
-		/**
-		 * Hold the name of the original annotation
-		 */
-		protected String name;
+    public static Elements select(Val annotation, Elements elements,
+            AnnotatedElement member) {
+        return select(new SelectBean(annotation), elements, member);
+    }
 
-		protected String select;
+    @Data
+    public static class SelectBean {
 
-		protected int index;
+        /**
+         * Hold the name of the original annotation
+         */
+        protected String name;
 
-		protected boolean unique;
+        protected String select;
 
-		protected boolean first;
+        protected int index;
 
-		protected boolean last;
+        protected boolean unique;
 
-		protected boolean optional;
+        protected boolean first;
 
-		public SelectBean(Select annotation) {
-			setAnnotation(annotation);
-			this.select = or(annotation.value(), annotation.select());
-			this.index = annotation.index();
-			this.unique = annotation.unique();
-			this.first = annotation.first();
-			this.last = annotation.last();
-			this.optional = annotation.optional();
-		}
+        protected boolean last;
 
-		public SelectBean(Text annotation) {
-			setAnnotation(annotation);
-			this.select = annotation.select();
-			this.index = annotation.index();
-			this.unique = annotation.unique();
-			this.first = annotation.first();
-			this.last = annotation.last();
-			this.optional = annotation.optional();
-		}
+        protected boolean optional;
 
-		public SelectBean(Attr annotation) {
-			setAnnotation(annotation);
-			this.select = annotation.select();
-			this.index = annotation.index();
-			this.unique = annotation.unique();
-			this.first = annotation.first();
-			this.last = annotation.last();
-			this.optional = annotation.optional();
-		}
+        public SelectBean(Select annotation) {
+            setAnnotation(annotation);
+            this.select = or(annotation.value(), annotation.select());
+            this.index = annotation.index();
+            this.unique = annotation.unique();
+            this.first = annotation.first();
+            this.last = annotation.last();
+            this.optional = annotation.optional();
+        }
 
-		public SelectBean(TagName annotation) {
-			setAnnotation(annotation);
-			this.select = annotation.select();
-			this.index = annotation.index();
-			this.unique = annotation.unique();
-			this.first = annotation.first();
-			this.last = annotation.last();
-			this.optional = annotation.optional();
-		}
+        public SelectBean(Text annotation) {
+            setAnnotation(annotation);
+            this.select = annotation.select();
+            this.index = annotation.index();
+            this.unique = annotation.unique();
+            this.first = annotation.first();
+            this.last = annotation.last();
+            this.optional = annotation.optional();
+        }
 
-		public SelectBean(Val annotation) {
-			setAnnotation(annotation);
-			this.select = annotation.select();
-			this.index = annotation.index();
-			this.unique = annotation.unique();
-			this.first = annotation.first();
-			this.last = annotation.last();
-			this.optional = annotation.optional();
-		}
+        public SelectBean(Attr annotation) {
+            setAnnotation(annotation);
+            this.select = annotation.select();
+            this.index = annotation.index();
+            this.unique = annotation.unique();
+            this.first = annotation.first();
+            this.last = annotation.last();
+            this.optional = annotation.optional();
+        }
 
-		protected void setAnnotation(Annotation annotation) {
-			this.name = annotation.annotationType().getSimpleName();
-		}
+        public SelectBean(TagName annotation) {
+            setAnnotation(annotation);
+            this.select = annotation.select();
+            this.index = annotation.index();
+            this.unique = annotation.unique();
+            this.first = annotation.first();
+            this.last = annotation.last();
+            this.optional = annotation.optional();
+        }
 
-		private String or(String value1, String value2) {
-			return StringUtil.isBlank(value1) ? value2 : value1;
-		}
-	}
+        public SelectBean(Val annotation) {
+            setAnnotation(annotation);
+            this.select = annotation.select();
+            this.index = annotation.index();
+            this.unique = annotation.unique();
+            this.first = annotation.first();
+            this.last = annotation.last();
+            this.optional = annotation.optional();
+        }
 
+        protected void setAnnotation(Annotation annotation) {
+            this.name = annotation.annotationType().getSimpleName();
+        }
+
+        private String or(String value1, String value2) {
+            return StringUtil.isBlank(value1) ? value2 : value1;
+        }
+    }
 
 }
--- src/test/java/estivate/RecursiveTest.java
+++ src/test/java/estivate/RecursiveTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-import estivate.EstivateMapper;
+import estivate.annotations.Attr;
 import estivate.annotations.Select;
 import estivate.annotations.Text;
 import lombok.Data;
@@ -18,6 +18,61 @@ public class RecursiveTest {
     EstivateMapper mapper = new EstivateMapper();
 
     @Test
+    public void selectRec3() {
+
+        InputStream document = read("/select/u2.html");
+
+        Result3 result = mapper.map(document, Result3.class);
+
+        Assert.assertNotNull(result);
+
+        Assert.assertNotNull(result.getSubResults());
+        Assert.assertEquals(3, result.getSubResults().size());
+        Assert.assertEquals("role_1", result.getSubResults().get(0).getRole());
+        Assert.assertEquals("role_2", result.getSubResults().get(1).getRole());
+        Assert.assertEquals("role_3", result.getSubResults().get(2).getRole());
+
+        log.info(result.toString());
+    }
+
+    @Test
+    public void selectRec3Bis() {
+
+        InputStream document = read("/select/u2.html");
+
+        Result3 result = mapper.map(document, Result3.class);
+
+        Assert.assertNotNull(result);
+
+        Assert.assertNotNull(result.getSubResults());
+        Assert.assertEquals(3, result.getSubResults().size());
+        Assert.assertEquals("Name 1", result.getSubResults().get(0).getName());
+        Assert.assertEquals("Name 2", result.getSubResults().get(1).getName());
+        Assert.assertEquals("Name 3", result.getSubResults().get(2).getName());
+
+        log.info(result.toString());
+    }
+
+    @Data
+    public static class Result3 {
+
+        @Select(".name")
+        public List<SubResult> subResults;
+
+        @Data
+        public static class SubResult {
+
+            @Attr("role")
+            String role;
+
+            @Text
+            String name;
+
+        }
+
+    }
+
+    @Test
     public void selectRec1() {
 
         InputStream document = read("/select/u2.html");
@@ -25,12 +80,12 @@ public class RecursiveTest {
         ResultSingle result = mapper.map(document, ResultSingle.class);
 
         Assert.assertNotNull(result);
-        
+
         Assert.assertNotNull(result.getSubResult());
         Assert.assertNotNull(result.getSubResult2());
-        
-        Assert.assertEquals("Name 2",result.getSubResult().getName());
-        Assert.assertEquals("Name 2",result.getSubResult2().getName());
+
+        Assert.assertEquals("Name 2", result.getSubResult().getName());
+        Assert.assertEquals("Name 2", result.getSubResult2().getName());
 
         log.info(result.toString());
     }
@@ -43,23 +98,23 @@ public class RecursiveTest {
         ResultList result = mapper.map(document, ResultList.class);
 
         Assert.assertNotNull(result);
-        
+
         Assert.assertNotNull(result.getSubResults());
         Assert.assertNotNull(result.getSubResults2());
-        
+
         Assert.assertEquals(3, result.getSubResults().size());
         Assert.assertEquals(3, result.getSubResults2().size());
 
         List<SubResult> subResults = result.getSubResults();
         for (SubResult subResult : subResults) {
-			subResult.getName();
-		}
-        
+            subResult.getName();
+        }
+
         List<SubResult> subResults2 = result.getSubResults2();
         for (SubResult subResult : subResults2) {
-        	subResult.getName();
+            subResult.getName();
         }
-        
+
         log.info(result.toString());
 
     }
@@ -69,12 +124,12 @@ public class RecursiveTest {
 
         @Select("#div2")
         public SubResult subResult;
-        
-		public SubResult subResult2;
-        
+
+        public SubResult subResult2;
+
         @Select("#div2")
-        public void setSubResult2(SubResult subResult2){
-			this.subResult2 = subResult2;
+        public void setSubResult2(SubResult subResult2) {
+            this.subResult2 = subResult2;
         }
 
     }
@@ -85,19 +140,19 @@ public class RecursiveTest {
         @Select(".someClass")
         public List<SubResult> subResults;
 
-		public List<SubResult> subResults2;
-        
-		@Select(".someClass")
-        public void setSubResult2(List<SubResult> subResults){
-			subResults2 = subResults;
+        public List<SubResult> subResults2;
+
+        @Select(".someClass")
+        public void setSubResult2(List<SubResult> subResults) {
+            subResults2 = subResults;
         }
-        
+
     }
 
     @Data
     public static class SubResult {
 
-    	@Text(select=".name")
+        @Text(select = ".name")
         public String name;
 
     }
