1f39168d28e67144d30472b71a2121231d1e0eef
Benoit <benoit.theunissen@gmail.com>
2016-04-03 13:57:51 +0200

    add tests for @Val

--- src/test/java/estivate/ValTest.java
+++ src/test/java/estivate/ValTest.java
@@ -1,11 +1,11 @@
 package estivate;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import estivate.EstivateMapper;
 import estivate.annotations.Val;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +16,36 @@ public class ValTest {
     EstivateMapper mapper = new EstivateMapper();
 
     @Test
+    public void val2() {
+
+        InputStream document = read("/val/u2.html");
+
+        Result2 result = mapper.map(document, Result2.class);
+
+        Assert.assertNotNull(result);
+
+        Assert.assertEquals("My Value 1 My Value 2 My Value 3",
+                result.getValue());
+
+        Assert.assertEquals(3, result.getValues().size());
+        Assert.assertEquals("My Value 1", result.getValues().get(0));
+        Assert.assertEquals("My Value 2", result.getValues().get(1));
+        Assert.assertEquals("My Value 3", result.getValues().get(2));
+
+        log.info(result.toString());
+    }
+
+    @Data
+    public static class Result2 {
+
+        @Val(select = "input")
+        public String value;
+
+        @Val(select = "input")
+        public List<String> values;
+    }
+
+    @Test
     public void val1() {
 
         InputStream document = read("/val/u1.html");
@@ -23,22 +53,22 @@ public class ValTest {
         Result result = mapper.map(document, Result.class);
 
         Assert.assertNotNull(result);
-        
+
         Assert.assertEquals("My Value", result.getValue1());
         Assert.assertEquals("My Value", result.getValue2());
-        
+
         log.info(result.toString());
     }
 
     @Data
     public static class Result {
 
-    	@Val(select="#name")
+        @Val(select = "#name")
         public String value1;
 
         public String value2;
 
-        @Val(select="#name")
+        @Val(select = "#name")
         public void setValue2(String value2) {
             this.value2 = value2;
         }
