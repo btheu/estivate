e159f546817688b8764d7d08bd82158ca96a0df7
Benoit <benoit.theunissen@gmail.com>
2016-04-03 23:00:40 +0200

    add sof tests

---
+++ src/test/java/estivate/Sof1.java
@@ -0,0 +1,53 @@
+package estivate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Assert;
+import org.junit.Test;
+
+import estivate.annotations.Text;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Sof1 {
+
+    EstivateMapper mapper = new EstivateMapper();
+
+    @Test
+    public void sof_36333383() throws IOException {
+        // http://stackoverflow.com/questions/36333383/data-scraping-for-an-android-app-from-a-local-html-page
+
+        InputStream document = read("/sof/u36333383.html");
+
+        Document doc = Jsoup.parse(document, "UTF-8", "/");
+
+        Result_36333383 result = mapper.map(doc, Result_36333383.class);
+
+        Assert.assertNotNull(result);
+
+        log.info(result.toString());
+    }
+
+    @Data
+    public static class Result_36333383 {
+
+        @Text(select = "font:matchesOwn(.*:.*)")
+        public List<String> times;
+
+        @Text(select = "font:not(:matchesOwn(.*:.*))")
+        public List<String> others;
+
+    }
+
+    private InputStream read(String string) {
+        InputStream resourceAsStream = EstivateMapper.class
+                .getResourceAsStream(string);
+        Assert.assertNotNull(resourceAsStream);
+        return resourceAsStream;
+    }
+}
