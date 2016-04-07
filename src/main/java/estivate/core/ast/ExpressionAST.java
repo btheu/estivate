08ef5ec1d29ce4d506ef91dc9f628aef34e1a680
Benoit <benoit.theunissen@gmail.com>
2016-04-07 20:04:35 +0200

    add converter on expressionAST

--- src/main/java/estivate/core/ast/ExpressionAST.java
+++ src/main/java/estivate/core/ast/ExpressionAST.java
@@ -5,10 +5,12 @@ import lombok.Data;
 @Data
 public abstract class ExpressionAST {
 
+    protected Boolean optional;
+    
 	protected QueryAST query;
 
 	protected ReduceAST reduce;
 
-	protected Boolean optional;
+	protected ConverterAST converter;
 
 }
