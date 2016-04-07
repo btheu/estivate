771652698640999e7037d178f092c527b7d4c041
benoit theunissen <benoit.theunissen@gmail.com>
2016-04-07 12:36:17 +0200

    fix simple converter

--- src/main/java/estivate/core/impl/SimpleConverter.java
+++ src/main/java/estivate/core/impl/SimpleConverter.java
@@ -5,14 +5,20 @@ import org.jsoup.select.Elements;
 import estivate.core.ClassUtils;
 import estivate.core.Converter;
 
-public class StandardConverter implements Converter {
+/**
+ * 
+ * 
+ * @author Benoit Theunissen
+ *
+ */
+public class SimpleConverter implements Converter {
 
 	public boolean canConvert(Object value, Class<?> targetType) {
 		return ClassUtils.isAssignableValue(targetType, value) && !Elements.class.equals(value.getClass());
 	}
 
 	public Object convert(Object value, Class<?> targetType) {
-		if (ClassUtils.isAssignableValue(targetType, value)) {
+		if (ClassUtils.isAssignableValue(targetType, value) && !Elements.class.equals(value.getClass())) {
 			return value;
 		}
 		return null;
