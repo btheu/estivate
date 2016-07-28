3b37cd00ecde579366f2c8ea134a5903968855de
benoit theunissen <benoit.theunissen@cgi.com>
2016-07-28 18:56:22 +0200

    test:add reduce with select annotations testcases

--- src/test/java/estivate/AttrTest.java
+++ src/test/java/estivate/AttrTest.java
@@ -7,22 +7,25 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-import estivate.annotations.Attr;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import estivate.annotations.Attr;
+import estivate.annotations.Select;
+
 @Slf4j
-public class AttrTest extends EstivateTest { 
+public class AttrTest extends EstivateTest {
 
     @Test
     public void attr2() throws IOException {
 
-        InputStream document = read("/attr/u2.html");
+        InputStream document = this.read("/attr/u2.html");
 
-        Result2 result = mapper.map(document, Result2.class);
+        Result2 result = this.mapper.map(document, Result2.class);
 
         Assert.assertNotNull(result);
-        Assert.assertEquals("role_1 role_2 role_3", result.getRole());
+        Assert.assertEquals("role_1 role_2 role_3", result.getRole1());
+        Assert.assertEquals("role_1 role_2 role_3", result.getRole2());
 
         Assert.assertEquals(3, result.getRoles().size());
         Assert.assertEquals("role_1", result.getRoles().get(0));
@@ -37,7 +40,11 @@ public class AttrTest extends EstivateTest {
     public static class Result2 {
 
         @Attr(select = "div", value = "role")
-        public String role;
+        public String role1;
+
+        @Attr("role")
+        @Select(select = "div")
+        public String role2;
 
         @Attr(select = "div", value = "role")
         public List<String> roles;
@@ -47,13 +54,14 @@ public class AttrTest extends EstivateTest {
     @Test
     public void attr1() throws IOException {
 
-        InputStream document = read("/attr/u1.html");
+        InputStream document = this.read("/attr/u1.html");
 
-        Result result = mapper.map(document, Result.class);
+        Result result = this.mapper.map(document, Result.class);
 
         Assert.assertNotNull(result);
         Assert.assertEquals("role_of_name", result.getName1());
         Assert.assertEquals("role_of_name", result.getName2());
+        Assert.assertEquals("role_of_name", result.getName3());
 
         log.info(result.toString());
 
@@ -65,11 +73,15 @@ public class AttrTest extends EstivateTest {
         @Attr(select = "#name", value = "role")
         public String name1;
 
+        @Attr("role")
+        @Select(select = "#name")
         public String name2;
 
+        public String name3;
+
         @Attr(select = "#name", value = "role")
-        public void setName2(String name2) {
-            this.name2 = name2;
+        public void setName3(String name3) {
+            this.name3 = name3;
         }
 
     }
--- src/test/java/estivate/TagNameTest.java
+++ src/test/java/estivate/TagNameTest.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-import estivate.annotations.TagName;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+
+import estivate.annotations.Select;
+import estivate.annotations.TagName;
 
 @Slf4j
 public class TagNameTest extends EstivateTest {
@@ -17,13 +19,14 @@ public class TagNameTest extends EstivateTest {
     @Test
     public void tagName2() throws IOException {
 
-        InputStream document = read("/tagName/u2.html");
+        InputStream document = this.read("/tagName/u2.html");
 
-        Result2 result = mapper.map(document, Result2.class);
+        Result2 result = this.mapper.map(document, Result2.class);
 
         Assert.assertNotNull(result);
 
-        Assert.assertEquals("input div span", result.getTagName());
+        Assert.assertEquals("input div span", result.getTagName1());
+        Assert.assertEquals("input div span", result.getTagName2());
 
         Assert.assertEquals(3, result.getTagNames().size());
         Assert.assertEquals("input", result.getTagNames().get(0));
@@ -37,7 +40,11 @@ public class TagNameTest extends EstivateTest {
     public static class Result2 {
 
         @TagName(select = ".tag")
-        public String tagName;
+        public String tagName1;
+
+        @TagName
+        @Select(".tag")
+        public String tagName2;
 
         @TagName(select = ".tag")
         public List<String> tagNames;
@@ -47,14 +54,15 @@ public class TagNameTest extends EstivateTest {
     @Test
     public void tagName1() throws IOException {
 
-        InputStream document = read("/tagName/u1.html");
+        InputStream document = this.read("/tagName/u1.html");
 
-        Result result = mapper.map(document, Result.class);
+        Result result = this.mapper.map(document, Result.class);
 
         Assert.assertNotNull(result);
 
         Assert.assertEquals("input", result.getTagName1());
         Assert.assertEquals("input", result.getTagName2());
+        Assert.assertEquals("input", result.getTagName3());
 
         log.info(result.toString());
     }
@@ -65,11 +73,15 @@ public class TagNameTest extends EstivateTest {
         @TagName(select = "#name")
         public String tagName1;
 
+        @TagName
+        @Select("#name")
         public String tagName2;
 
+        public String tagName3;
+
         @TagName(select = "#name")
-        public void setTagName2(String tagName2) {
-            this.tagName2 = tagName2;
+        public void setTagName3(String tagName3) {
+            this.tagName3 = tagName3;
         }
 
     }
